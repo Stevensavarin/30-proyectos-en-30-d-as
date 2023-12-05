202463d47164318adfe6c82583b6bb9b8e711f0d
steven_savarin <srssdesing@gmail.com>
2023-12-05 21:13:38 +0100

    new project

---
+++ 20-accordion/script.js
@@ -0,0 +1,30 @@
+class Acordion{
+    constructor(selector, multiple=true){
+        this.accordion = document.querySelector(selector);
+        this.multiple = multiple;
+        this.bindEvents();
+    }
+
+    bindEvents(){
+        this.accordion.querySelectorAll(".item header").forEach(itemHeader =>{
+
+            itemHeader.addEventListener("click", ()=>{
+                let item = itemHeader.parentNode;
+                this.validateMultiple(item);
+                item.classList.toggle("active");
+            })
+        });
+    }
+    validateMultiple(selectorItem){
+        if(this.multiple) return;
+
+        this.accordion.querySelectorAll(".item").forEach(item => {
+            if(selectedItem == item) return;
+            item.classList.remove("active")
+        })
+    }
+}
+
+(function(){
+    new Acordion(".accordion")
+})()
